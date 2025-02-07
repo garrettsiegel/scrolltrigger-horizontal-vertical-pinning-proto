bc9b8bb6f20770e75829202cfb44d05de6882f85
garrettsiegel <garrett@garrettsiegel.com>
2025-02-06 21:47:48 -0500

    Naming conventions

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import "./App.css";
-
-gsap.registerPlugin(ScrollTrigger);
+import { useEffect, useRef } from "react"
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import "./App.css"
+gsap.registerPlugin(ScrollTrigger)
 
 export function App() {
-  const racesRef1 = useRef(null);
-  const racesRef2 = useRef(null);
+  const horizontalSectionRef1 = useRef(null)
+  const horizontalSectionRef2 = useRef(null)
 
   useEffect(() => {
     const sections = [
-      { ref: racesRef1, trigger: ".racesWrapper1" },
-      { ref: racesRef2, trigger: ".racesWrapper2" },
-    ];
+      { ref: horizontalSectionRef1, trigger: ".horiz-section-1" },
+      { ref: horizontalSectionRef2, trigger: ".horiz-section-2" },
+    ]
 
     sections.forEach(({ ref, trigger }) => {
-      const races = ref.current;
-      if (!races) return;
+      const horizontalSection = ref.current
+      if (!horizontalSection) return
 
       function getScrollAmount() {
-        return -(races.scrollWidth - window.innerWidth);
+        return -(horizontalSection.scrollWidth - window.innerWidth)
       }
 
-      const tween = gsap.to(races, {
+      const tween = gsap.to(horizontalSection, {
         x: getScrollAmount,
         duration: 3,
         ease: "none",
-      });
+      })
 
       ScrollTrigger.create({
         trigger,
@@ -38,53 +37,51 @@ export function App() {
         scrub: 1,
         invalidateOnRefresh: true,
         markers: true,
-      });
-    });
+      })
+    })
 
     return () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    };
-  }, []);
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+    }
+  }, [])
 
   return (
     <section>
-
-      <div className="racesWrapper racesWrapper1">
-        <div className="races" ref={racesRef1}>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
+      <div className="horiz-section-1">
+        <div className="horiz-section" ref={horizontalSectionRef1}>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
         </div>
       </div>
 
-      <div className="space-100vh lightBG">
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
+      <div className="vert-section">
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
       </div>
 
-      <div className="racesWrapper racesWrapper2">
-        <div className="races" ref={racesRef2}>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
-          <div className="h-box">horizontal section</div>
+      <div className="horiz-section-2">
+        <div className="horiz-section" ref={horizontalSectionRef2}>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
+          <div className="horiz-box">horizontal section</div>
         </div>
       </div>
 
-      <div className="space-100vh lightBG">
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
-        <div className="v-box">vertical section</div>
+      <div className="vert-section">
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
+        <div className="vert-box">vertical section</div>
       </div>
-
     </section>
-  );
+  )
 }
